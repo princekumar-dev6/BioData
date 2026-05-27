9922f73155cc4347916cf21a530080eb2524dee4
Prince Kumar <princekumar@Princes-MacBook-Air.local>
2026-05-27 08:20:17 +0530

    added favicon icons

--- app/layout.js
+++ app/layout.js
@@ -136,9 +136,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${jakarta.variable} ${cormorant.variable}`}>
       <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/android-chrome-192x192.png" />
-        <link rel="apple-touch-icon" href="/android-chrome-512x512.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZBXRW4SW4Z" />
         <script
           dangerouslySetInnerHTML={{
